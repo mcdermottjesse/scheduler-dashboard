02ea146a5b8a44e9e259fd87e8495662972dbbdf
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-03-25 16:58:13 -0700

    add component lifecycles and websockets

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,73 +1,122 @@
 import React, { Component } from "react";
+import axios from "axios";
 import classnames from "classnames";
-import Loading from "./Loading"
-import Panel from "./Panel"
+import Loading from "./Loading";
+import Panel from "./Panel";
+import { setInterview } from "helpers/reducers";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
-  }
+    getValue: getInterviewsPerDay,
+  },
 ];
 
-
 class Dashboard extends Component {
+  state = {
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviews: {},
+  };
 
-  state = { loading: false, focused: null };
+  componentDidMount() {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+      });
+      this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+      this.socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+
+        if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+          this.setState((previousState) =>
+            setInterview(previousState, data.id, data.interview)
+          );
+        }
+      };
+    });
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
 
   selectPanel(id) {
-    this.setState(prevState => ({
-     focused: prevState.focused !== null ? null : id
-    }))
-   }
-
+    this.setState((prevState) => ({
+      focused: prevState.focused !== null ? null : id,
+    }));
+  }
   render() {
     const dashboardClasses = classnames("dashboard", {
-      "dashboard--focused": this.state.focused
-     });
+      "dashboard--focused": this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
-   
+
     const panels = data
-    .filter(
-      panel => this.state.focused === null || this.state.focused === panel.id
-     )
-  
-    .map(panel => {
-      return(
-      <Panel 
-      key={panel.id}
-      id={panel.id}
-      label={panel.label}
-      value={panel.value}
-      onSelect={event => this.selectPanel(panel.id)}
-    
-      />
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
       )
-    })
+
+      .map((panel) => {
+        return (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.getValue(this.state)}
+            onSelect={(event) => this.selectPanel(panel.id)}
+          />
+        );
+      });
 
     return <main className={dashboardClasses}>{panels}</main>;
-    
   }
 }
-
 
 export default Dashboard;
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -5,10 +5,7 @@ class Panel extends Component {
     const { label, value, onSelect } = this.props;
 
     return (
-      
-      <section
-        className="dashboard__panel" onClick={onSelect}
-      >
+      <section className="dashboard__panel" onClick={onSelect}>
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
